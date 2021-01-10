9b7112204f0974260deb233fc9c9b1c6096310ea
Promise Xu <promise@klido.me>
2021-01-10 00:18:37 -0500

    Endpoint testing WIP

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+// import stoppable from 'stoppable';
+// import http from 'http';
+import 'express-async-errors';
+
+import * as imagesRoute from './routes/images';
+
+const app = express();
+
+app.get('/', (_req, res) => {
+    res.send('This is the Image Processing API');
+});
+app.use(imagesRoute.BASE_PATH, imagesRoute.default);
+
+// Error handling
+app.use((err: Error, _req: any, res: any, _next: any) => {
+    if (err.message === 'Source image does not exist') {
+        res.status(404);
+    } else {
+        res.status(500);
+    }
+    res.send(err.message);
+});
+
+export default app;
